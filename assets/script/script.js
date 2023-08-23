8012b4e4651846c370c2e71bfe282502292d1e41
Davi (Estudo) <daviamorim_sz@live.com>
2023-08-23 02:51:18 -0300

    alura_lp_Fruit&Fruit

---
+++ assets/script/script.js
@@ -0,0 +1,23 @@
+const navAboveAll = document.getElementById("navWrapper");
+const menuBurguer = document.getElementById("burguerMenu");
+
+const animationToRight = "navWAnimationToRight";
+const animationToLeft = "navWAnimationToLeft";
+
+function rodaMenu() {
+  function hideMenu() {
+    navAboveAll.style.display = "none";
+  }
+  if (navAboveAll.style.display == "none") {
+    menuBurguer.textContent = "close";
+    navAboveAll.classList.remove(animationToRight);
+    navAboveAll.classList.add(animationToLeft);
+    navAboveAll.style.display = "flex";
+  } else {
+    menuBurguer.textContent = "menu";
+    navAboveAll.classList.remove(animationToLeft);
+    navAboveAll.classList.add(animationToRight);
+    setTimeout(hideMenu, 500);
+  }
+}
+menuBurguer.addEventListener("click", rodaMenu);
